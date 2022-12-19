eb00e2766088688c671939de66b7336f3f395730
Renzo Yanzon <renzoyanzon@gmail.com>
2022-12-19 20:58:02 -0300

    desafio ejs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const indexRouter = require("./src/routes/index");
 const errorMiddleware = require("./src/middlewares/errorMiddleware")
-
+const _=require("lodash");
 require("dotenv").config();
 
 const app = express();
@@ -9,8 +9,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const products= [];
+
 app.use("/api", indexRouter);
 
 app.use("/static",express.static(__dirname + "/public"));
+
+//desafio EJS
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (_req,res)=>{
+    res.render("pages/form")
+});
+
+app.get("/productos", (_req,res)=>{
+    res.render("pages/view", {products})
+})
+
+app.post("/productos", (req, res) => {
+    const { title, price, thumbnail } = req.body;
+    if (_.isNil(title) || _.isNil(price) || _.isNil(thumbnail)){
+        res.render("error", {message: "Hay un problema con las variables"})
+    } else {
+        products.push({title, price, thumbnail });
+       res.render("pages/form", {products});
+    }
+})
+
 
 module.exports = app;

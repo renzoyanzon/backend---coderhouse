fe02d1dc63163e29943777f4d6ea89166de481c2
Renzo Yanzon <renzoyanzon@gmail.com>
2023-02-07 21:06:18 -0300

    Agregando url de mongoose

--- src/daos/productsDao/mongooseProducts.service.js
+++ src/daos/productsDao/mongooseProducts.service.js
@@ -9,12 +9,22 @@ class ProductsClass{
     }
 
     async createConnection(){
+
+        let MONGO_URI;
+        const MONGO_USER = process.env.MONGO_USER;
+        const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+        const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+        const MONGO_QUERY = process.env.MONGO_QUERY;
+        const MONGO_HOST = process.env.MONGO_HOST;
+
+        if (!MONGO_USER){
+            MONGO_URI = `${process.env.MONGO_URI}/${MONGO_DB_NAME}`
+        } else {
+            MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB_NAME}?${MONGO_QUERY}`  
+        }
+
         try{
-            const mongodb_URL = process.env.MONGODB_URL;
-            const mongodb_db = process.env.MONGODB_DB;
-            const URL = `${mongodb_URL}/${mongodb_db}`;
-            console.log(URL)
-            await mongoose.connect(URL, {
+            await mongoose.connect(MONGO_URI, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             });
